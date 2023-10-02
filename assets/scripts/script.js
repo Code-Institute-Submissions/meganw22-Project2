007703b10edd4d74d5f90c0d8d8b1b3f601e4f36
Megan Walford <mwalford22@gmail.com>
2023-10-02 13:59:49 +0000

    add script src to html and overlay html/css

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// general identifiers
+console.log("hello");
+
+
+// Post DOM load, start up checks;
+document.addEventListener('DOMContentLoaded', function() {
+    let gameSection = document.getElementById('game-section');
+    
+})
